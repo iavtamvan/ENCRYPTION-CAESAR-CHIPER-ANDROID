b771e2f142ee82ac1731a4cfcb7e663d1da8a0f9
Khoirul02 <hudak006@gmail.com>
2019-09-18 13:56:28 +0700

    tambah icon plain text, icon encrypt,aksi tambah dan kurang key dengan image plus dan minus v-1

--- app/src/main/java/com/iavariav/enkripsi/MainActivity.java
+++ app/src/main/java/com/iavariav/enkripsi/MainActivity.java
@@ -1,10 +1,12 @@
 package com.iavariav.enkripsi;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,20 +17,46 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText edtPlain;
     private EditText edtEncrypt;
-    private TextView tvHasil;
+//    private TextView tvHasil;
     private Button btnSubmit;
+    private ImageView ivPlus;
+    private ImageView ivMinus;
 
     String newString, inputString;
-    int key;
+//    int key;
     private EditText edtKey;
     private Switch sw;
-
+    private int key = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+
+        edtKey.setText(""+key);
+        ivPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edtKey.getText().toString().isEmpty()){
+                    resetKey();
+                    tambahKey();
+                }else {
+                    tambahKey();
+                }
+            }
+        });
+        ivMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edtKey.getText().toString().isEmpty()){
+                    resetKey();
+                    kurangKey();
+                }else {
+                    kurangKey();
+                }
+            }
+        });
 
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,7 +69,34 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
+    private void resetKey() {
+        key = 0;
+        edtKey.setText(""+key);
+    }
+    private void tambahKey() {
+        try {
+            key = Integer.parseInt(edtKey.getText().toString());
+            key = key + 1;
+            edtKey.setText(""+key);
+        }catch (NumberFormatException nfe){
+            resetKey();
+            tambahKey();
+        }
+    }
+    private void kurangKey() {
+        try {
+            key = Integer.parseInt(edtKey.getText().toString());
+            if (key==0){
+                Toast.makeText(this, "Barang tidak bisa kurang dari 0", Toast.LENGTH_SHORT).show();
+            }else {
+                key = key- 1;
+                edtKey.setText(""+key);
+            }
+        }catch (NumberFormatException nfe){
+            resetKey();
+            kurangKey();
+        }
+    }
 
     private void Caesar_cipher_coding_method(int shift) {
         Editable msg = edtPlain.getText();
@@ -79,9 +134,11 @@ public class MainActivity extends AppCompatActivity {
     private void initView() {
         edtPlain = findViewById(R.id.edt_plain);
         edtEncrypt = findViewById(R.id.edt_encrypt);
-        tvHasil = findViewById(R.id.tv_hasil);
+//        tvHasil = findViewById(R.id.tv_hasil);
         btnSubmit = findViewById(R.id.btn_submit);
         edtKey = findViewById(R.id.edt_key);
         sw = findViewById(R.id.sw);
+        ivPlus = findViewById(R.id.icon_plus);
+        ivMinus = findViewById(R.id.icon_minus);
     }
 }
